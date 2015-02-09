fd27d0ca1280c8d2a862d999901bb4d41fb755a7
Richard Deloge <r.deloge@uni-alteri.com>
2015-02-09 12:50:42 +0100

    update desc

--- wp_sellsy.php
+++ wp_sellsy.php
@@ -4,8 +4,8 @@ Plugin Name: WP Sellsy
 Plugin URI: http://www.sellsy.com/
 Description: Le plugin WP prospection Sellsy vous permet d'afficher un formulaire de contact connecté avec votre compte Sellsy. Quand le formulaire sera soumis, un prospect et (optionnellement) une opportunité seront créées sur votre compte Sellsy. Pour activer le plugin, vous devez insérer ci-dessous vos tokens d'API Sellsy, disponibles depuis Réglages puis Accès API. Pour afficher le formulaire sur une page ou dans un post insérez le code [wpsellsy].
 Version: 1.1
-Author: <a href="mailto:contact@synaptech.fr">synaptech</a>
-Author URI: http://www.synaptech.fr
+Author: <a href="mailto:r.deloge@uni-alteri.com">Richard DELOGE, Uni Alteri</a>
+Author URI: http://agence.net.ua
 */
 
 require_once 'define.php';
