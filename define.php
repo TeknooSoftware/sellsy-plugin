60e01c96303c24433de45febaa957b719dab6427
Richard Deloge <r.deloge@uni-alteri.com>
2015-04-25 16:17:03 +0200

    define

--- define.php
+++ define.php
@@ -1,15 +1,27 @@
 <?php
-define('SELLSY_WP_VERSION', '1.0');
-define('SELLSY_WP_PATH', __DIR__);
-define('SELLSY_WP_PATH_FILE', SELLSY_WP_PATH.'/wp_sellsy.php');
-define('SELLSY_WP_PATH_INC', __DIR__.'/inc');
-define('SELLSY_WP_PATH_LANG', dirname(plugin_basename(__FILE__)).'/lang/');
-define('SELLSY_WP_FOLDER', basename(SELLSY_WP_PATH));
-define('SELLSY_WP_URL', plugins_url().'/'.SELLSY_WP_FOLDER);
-define('SELLSY_WP_URL_INCLUDES', SELLSY_WP_URL.'/inc');
-define('SELLSY_WP_API_URL', 'https://apifeed.sellsy.com/0/');
-define('SELLSY_WP_SOURCE_URL', 'https://www.sellsy.com/?_f=prospection_prefs&action=sources');
-define('SELLSY_WP_WEB_URL', 'https://www.sellsy.com/');
-define('SELLSY_WP_WEBAPI_URL', 'https://www.sellsy.com/?_f=prefsApi');
-define('SELLSY_WP_JQUERY_URL', 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js');
-define('SELLSY_WP_JQUERY_VERSION', '1.9.1');
+!defined('SELLSY_WP_VERSION')
+    && define('SELLSY_WP_VERSION', '1.0');
+
+!defined('SELLSY_WP_PATH')
+    && define('SELLSY_WP_PATH', __DIR__);
+
+!defined('SELLSY_WP_PATH_FILE')
+    && define('SELLSY_WP_PATH_FILE', SELLSY_WP_PATH.'/wp_sellsy.php');
+
+!defined('SELLSY_WP_PATH_INC')
+    && define('SELLSY_WP_PATH_INC', __DIR__.'/inc');
+
+!defined('SELLSY_WP_PATH_LANG')
+    && define('SELLSY_WP_PATH_LANG', dirname(plugin_basename(__FILE__)).'/lang/');
+
+!defined('SELLSY_WP_FOLDER')
+    && define('SELLSY_WP_FOLDER', basename(SELLSY_WP_PATH));
+
+!defined('SELLSY_WP_URL')
+    && define('SELLSY_WP_URL', plugins_url().'/'.SELLSY_WP_FOLDER);
+
+!defined('SELLSY_WP_API_URL')
+    && define('SELLSY_WP_API_URL', 'https://apifeed.sellsy.com/0/');
+
+!defined('SELLSY_WP_WEB_URL')
+    && define('SELLSY_WP_WEB_URL', 'https://www.sellsy.com/');
