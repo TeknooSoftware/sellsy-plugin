4fb406dcb6115eec92eeac0e78977ef901e534b3
Richard Deloge <r.deloge@uni-alteri.com>
2015-05-07 00:54:37 +0200

    writing test

--- tests/Wordpress/WpSellsyTest.php
+++ tests/Wordpress/WpSellsyTest.php
@@ -11,29 +11,27 @@ class WpSellsyTest extends \PHPUnit_Framework_TestCase
         $methodCalled = array();
         $methodArgs = array();
 
+        //To test closure
+        prepareMock(
+            'add_action',
+            '*',
+            function ($action, $arg) {
+                if ($arg instanceof \Closure) {
+                    $arg();
+                }
+            }
+        );
+
         include dirname(dirname(__DIR__)).'/wp_sellsy.php';
 
-        $this->assertEquals(
-            array (
-                'get_option',
-                'get_option',
-                'get_option',
-                'get_option',
-                'add_action',
-                'add_action',
-                'add_action',
-                'add_action',
-                'add_action',
-                'add_action',
-                'register_deactivation_hook',
-                'add_action',
-                'add_action',
-                'add_action',
-                'add_action',
-                'add_action',
-            ),
-            $methodCalled
-        );
+        $this->assertTrue(in_array('get_option', $methodCalled));
+        $this->assertTrue(in_array('register_setting', $methodCalled));
+        $this->assertTrue(in_array('add_filter', $methodCalled));
+        $this->assertTrue(in_array('register_deactivation_hook', $methodCalled));
+        $this->assertTrue(in_array('add_action', $methodCalled));
+        $this->assertTrue(in_array('add_settings_error', $methodCalled));
+        $this->assertTrue(in_array('add_shortcode', $methodCalled));
+        $this->assertTrue(in_array('register_widget', $methodCalled));
 
         $this->assertNotEmpty(
             $methodArgs
