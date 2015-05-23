d79073e49914447476fecdb832e697f6e30aa1e6
Richard Deloge <r.deloge@uni-alteri.com>
2015-05-23 19:41:44 +0200

    debuging

--- src/Wordpress/Widget.php
+++ src/Wordpress/Widget.php
@@ -109,7 +109,6 @@ class Widget extends \WP_Widget
             $afterWidget = $args['after_widget'];
         }
 
-        echo SELLSY_WP_PATH_INC.'/widget.php';
         if (!empty($out) && is_readable(SELLSY_WP_PATH_INC.'/widget.php')) {
             include SELLSY_WP_PATH_INC.'/widget.php';
         }
